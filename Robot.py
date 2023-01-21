ac091e55c91b162c7d3b60177b96e4be62b99849
Maxime Dautriche <maxdautriche@gmail.com>
2023-01-21 15:36:51 +0100

    Ajout du constructeur

--- Robot.py
+++ Robot.py
@@ -4,8 +4,19 @@ class Robot:
     """
 
     def __init__(self, nom: str, posX: float, posY: float, angle: float):
-        #code
-        return None
+        """
+        Constructeur de la classe Robot
+
+        Paramètres:
+        nom -> Nom du robot
+        posX -> position x du robot
+        posY -> position y du robot
+        angle -> orientation du robot (en degré)
+        """
+        self.nom = nom
+        self.posX = posX
+        self.posY = posY
+        self.angle = angle
 
     def avancer(self, distance: float):
         return None

80b491221bfb1df331aa3a777e2b102386b7c40a
Maxime Dautriche <maxdautriche@gmail.com>
2023-01-21 15:30:50 +0100

    Ajout du squelette de la classe Robot

---
+++ Robot.py
@@ -0,0 +1,35 @@
+class Robot:
+    """
+    Classe représentant un robot
+    """
+
+    def __init__(self, nom: str, posX: float, posY: float, angle: float):
+        #code
+        return None
+
+    def avancer(self, distance: float):
+        return None
+
+    def tourner(self, angle: float):
+        return None
+
+    def getPosition(self):
+        return None
+
+    def getX(self):
+        return None
+    
+    def getY(self):
+        return None
+
+    def getNom(self):
+        return None
+
+    def getAngle(self):
+        return None
+
+    def getDistanceFromRobot(self):
+        return None
+
+    def afficher(self, screen):
+        return None

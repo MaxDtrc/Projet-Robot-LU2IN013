84300558ec48ecb0aec728f1ce0985786a3e41d0
Maxime Dautriche <maxdautriche@gmail.com>
2023-01-22 14:36:51 +0100

    Ajout de la classe Terrain + constructeur

---
+++ Terrain.py
@@ -0,0 +1,27 @@
+class Terrain:
+    """
+    Classe représentant un terrain
+    """
+
+    def __init__(self, sizeX: int, sizeY: int):
+        """
+        Constructeur de la classe
+        
+        Paramètres:
+        sizeX -> taille X du terrain
+        sizeY -> taille Y du terrain
+        """
+        self._sizeX = sizeX
+        self._sizeY = sizeY
+    
+    def getSize(self):
+        return None
+
+    def getSizeX(self):
+        return None
+
+    def getSizeY(self):
+        return None
+
+    def afficher(self, screen):
+        return None

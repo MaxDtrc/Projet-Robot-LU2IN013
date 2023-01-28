3a93ae77207f4a4a42ba8bd01c33d7c9417592ee
Vinc20100 <vinc10370@gmail.com>
2023-01-28 14:18:52 +0100

    Création de la classe Obstacle

---
+++ Obstacle.py
@@ -0,0 +1,60 @@
+from math import cos, sin, radians, degrees
+from Terrain import Terrain
+from Robot import Robot
+from abc import ABC, abstractmethod
+
+class Obstacle(ABC): 
+    """
+    Classe abstraite représentant un obstacle
+    """
+    
+    def __init__(self, nom: str, posX: float, posY: float):
+        """
+        Constructeur de la classe Obstacle
+
+        Paramètres:
+        nom -> Nom de l'obstacle
+        posX -> position x du centre de l'obstacle 
+        posY -> position y du centre de l'obstacle
+        """
+        self._nom = nom
+        self._posX = posX
+        self._posY = posY
+
+        
+    @abstractmethod  
+    def testCrashRobot(self, robot : Robot):
+        pass
+        
+    def getNom(self):
+    	"""
+    	Renvoie le nom de l'obstacle"
+    	"""
+    	return self._nom
+  
+
+    def getPosition(self):
+        """
+        Renvoie un tuple contenant la position (x, y) de l'obstacle
+        """
+        return (self._posX, self._posY)
+
+    def getX(self):
+        """
+        Renvoie la position x du centre de l'obstacle
+        """
+        return self._posX
+    
+    def getY(self):
+        """
+        Renvoie la position y du centre de l'obstacle
+        """
+        return self._posY
+        
+    def setPosition(self, pX : float, pY: float):
+    	"""
+    	Modifie la position d'un obstacle
+    	"""
+    	self._posX = pX
+        self._posY = pY
+

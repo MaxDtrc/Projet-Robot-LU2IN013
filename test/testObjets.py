a00b8c19dcf8c9dcd00d604f7e20ba2f299670ac
Vinc20100 <vinc10370@gmail.com>
2023-02-04 14:34:50 +0100

    Tests unitaires

---
+++ test/testObjets.py
@@ -0,0 +1,101 @@
+import unittest
+
+from Objets import Robot, Obstacle, ObstacleRectangle, ObstacleRond
+
+class TestRobot(unittest.TestCase):
+    def setUp(self):
+        self.r = Robot("MJ", 20, 45, 0, 15, 200)
+
+    def testNom(self):
+        self.assertEqual(self.r.getNom(), "MJ")
+
+    def testGetX(self):
+        self.assertEqual(self.r.getX(), 20)
+
+    def testGetY(self):
+        self.assertEqual(self.r.getY(), 45)
+
+    def testGetPosition(self):
+        self.assertEqual(self.r.getPosition(), (20,45))
+
+    def testGetAngle(self):
+        self.assertEqual(self.r.getAngle(), 0)
+
+    def testGetRayon(self):
+        self.assertEqual(self.r.getRayon(), 15)
+
+    def testGetPosRoueGaucheX(self):
+        self.assertEqual(self.r.getPosRoueGaucheX(), 20)
+
+    def testGetPosRoueGaucheY(self):
+        self.assertEqual(self.r.getPosRoueGaucheY(), 60)
+
+    def testGetPosRoueDroiteX(self):
+        self.assertEqual(self.r.getPosRoueDroiteX(), 20)
+
+    def testGetPosRoueDroiteY(self):
+        self.assertEqual(self.r.getPosRoueDroiteY(), 30)
+
+    def testGetPosRoueGauche(self):
+        self.assertEqual(self.r.getPosRoueGauche(),(20,60))
+
+    def testGetPosRoueDroite(self):
+        self.assertEqual(self.r.getPosRoueDroite(),(20,30))
+    
+    def testGetVitesseDroite(self):
+        self.assertEqual(self.r.getVitesseDroite(), 0)
+
+    def testGetVitesseGauche(self):
+        self.assertEqual(self.r.getVitesseGauche(), 0)
+
+""" 
+getInfo()
+actualiser()
+accelererGauche()
+accelererDroite()
+accelerer()
+ralentirDroite()
+ralentirGauche()
+ralentir()
+setVitesseDroite()
+setVitesseGauche()
+setVitesse()
+"""
+
+class TestObstacleRond(unittest.TestCase):
+    def setUp(self):
+        self.obs = ObstacleRond("ObsOne", 100, 80, 10)
+
+    def testGetNom(self):
+        self.assertEqual(self.obs.getNom(), "ObsOne")
+
+    def testGetPosition(self):
+        self.assertEqual(self.obs.getPosition(), (100,80))
+    
+    def testGetX(self):
+        self.assertEqual(self.obs.getX(), 100)
+
+    def testGetY(self):
+        self.assertEqual(self.obs.getY(), 80)
+
+    def testGetRayon(self):
+        self.assertEqual(self.obs.getRayon(), 10)
+    
+    def testSetPosition(self):
+        self.obs.setPosition(-100, -80)
+        self.assertEqual(self.obs.getPosition(), (-100, -80))
+
+    def testEstDedans(self):
+        self.assertTrue(self.obs.estDedans(95,75))
+        self.assertFalse(self.obs.estDedans(30,50))
+
+    def testTestCrash(self):
+        r = Robot("Driftator", 95, 75, 0, 15, 200)
+        self.assertEqual(self.obs.testCrash(r), 1)
+        r1 = Robot("Driftator", 100, 80, 0, 15, 200)
+        self.assertEqual(self.obs.testCrash(r1), 1)
+        r2 = Robot("Driftator", 30, 50, 0, 15, 200)
+        self.assertEqual(self.obs.testCrash(r2), 0)
+
+if __name__ == "__main__":
+    unittest.main()

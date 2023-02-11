487635dffa7e01ff747e5187521bb69844ca4a18
lailabennani <laila.lahkim.bennani03@gmail.com>
2023-02-11 18:08:12 +0100

    ajout du controleur dans le init principal

--- robot/robot/__init__.py
+++ robot/robot/__init__.py
@@ -1,3 +1,4 @@
 from .objets import Robot, Terrain, Obstacle, ObstacleRectangle, ObstacleRond
 from .simulation import Simulation
 from .affichage import Affichage
+from .controleur import controleur, implemSimulation, implemVraiVie

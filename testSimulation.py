7c066009bdfc295d7d0812ff1680c00b216f1004
lailabennani <laila.lahkim.bennani03@gmail.com>
2023-02-11 18:38:15 +0100

    Ajout de l'API du robot, l'ia et du controleur

--- testSimulation.py
+++ testSimulation.py
@@ -1,43 +1,36 @@
 import robot as r
 
-import time
-import pygame
-import random
+#Initialisation du controleur
+controleur = r.controleur()
 
+try:
+    from robot2IN013 import Robot2IN013
+    implem = r.implemVraiVie(Robot2IN013())
+    controleur.changerImplementation(implem)
+except ImportError:
+    import pygame
+    import time
+    import random
 
-#Definition de la taille du terrain
-tailleTerrainX = 510
-tailleTerrainY = 510
+    #Definition de la taille du terrain
+    tailleTerrainX = 510
+    tailleTerrainY = 510
 
-#Definition de la "précision temporelle"
-dT = 0.1
-    
-#Creation de la simulation
-simulation = r.Simulation(dT)
-
-#Creation du terrain
-simulation.chargerJson('test.json')
-
-#Initialisation de l'affichage
-a = r.Affichage(simulation, 60, 1.5, True)
-
-
-
-simulation.start()
-a.start()
-
-#Boucle principale
-enMarche = True
-while enMarche:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            enMarche = False
+    #Definition de la "précision temporelle"
+    dT = 0.1
         
-    #Apparition d'un robot si aucun présent
-    if(simulation.getNombreDeRobots() == 0):
-        rob = r.Robot("robot", 0, 0, random.randint(0, 360), 7, 15, 10000)
-        rob.start()
-        simulation.ajouterRobot(rob)
-        
-    time.sleep(dT)
+    #Creation de la simulation
+    simulation = r.Simulation(dT)
 
+    #Creation du terrain
+    simulation.chargerJson('test.json')
+
+    #Initialisation de l'affichage
+    a = r.Affichage(simulation, 60, 1.5, True)
+    implem = r.implemSimulation(simulation.getRobot(0), simulation)
+    controleur.changerImplementation(implem)
+    simulation.start()
+    a.start()  
+
+ia = r.IA(controleur)
+ia.start()

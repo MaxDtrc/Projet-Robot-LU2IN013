9fdc9bef79db69fcd6f571806fae3759e26e3888
lailabennani <laila.lahkim.bennani03@gmail.com>
2023-02-11 18:36:56 +0100

    Ajout de l'ia dans le init principal

--- robot/robot/__init__.py
+++ robot/robot/__init__.py
@@ -2,3 +2,4 @@ from .objets import Robot, Terrain, Obstacle, ObstacleRectangle, ObstacleRond
 from .simulation import Simulation
 from .affichage import Affichage
 from .controleur import controleur, implemSimulation, implemVraiVie
+from .ia import IA

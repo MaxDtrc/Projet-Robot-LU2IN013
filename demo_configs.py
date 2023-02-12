826f6cce4e68a4b2889f6c6025898168b9ae22c0
Maxime Dautriche <maxdautriche@gmail.com>
2023-02-12 10:58:48 +0100

    Ajout d'un script demo_configs.py pour selectionner la config à charger

---
+++ demo_configs.py
@@ -0,0 +1,41 @@
+import robot as r
+import os
+
+
+#Instantiation du controleur
+controleur = r.controleur()
+
+#Definition de la taille du terrain
+tailleTerrainX = 510
+tailleTerrainY = 510
+
+#Definition de la "précision temporelle"
+dT = 0.1
+        
+#Creation de la simulation
+s = r.Simulation(dT)
+
+#Chargement des configs
+i = 1
+select = -1
+print("Liste des fichiers de config:")
+for cfg in os.listdir('config'):
+    print(str(i) + ": " + cfg[:-5])
+    i += 1
+
+while select < 1 or select > i:
+    select = int(input("Choisissez la configuration à charger: "))
+
+
+s.chargerJson("config/" + os.listdir('config')[select-1])
+
+#Initialisation de l'affichage
+a = r.Affichage(s, 60, 1.5, False)
+
+#Initialisation du controleur
+implem = r.implemSimulation(s.getRobot(0), s)
+controleur.changerImplementation(implem)
+
+#Start des threads de la simulation
+s.start()
+a.start()  

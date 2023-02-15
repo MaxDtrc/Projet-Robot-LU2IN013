cedbf74b446949af94c178c2286d00c4bfb3c3af
Laurent <laurentlaurentchen@gmail.com>
2023-02-15 10:10:16 +0100

    Ajout TestRetirerRobot

--- test/testSimulation.py
+++ test/testSimulation.py
@@ -9,6 +9,7 @@ class TestSimulation(unittest.TestCase):
         robot1 = Robot("Andrew", 0, 0, 0, 5, 10, 200)
         liste_robot=[robot1]
         self.s = Simulation(1,liste_robot,terrain)
+        #La simulation contient 1 robot "Andrew"
 
     def TestGetNombreDeRobots(self):
         self.assertEqual(self.s.getNombreDeRobots(),1)
@@ -18,15 +19,18 @@ class TestSimulation(unittest.TestCase):
         self.s.ajouterRobot(robot2)
         self.assertEqual(self.s.getNombreDeRobots(),2)
 
+    def TestRetirerRobot(self):
+        robot1 = Robot("Andrew", 0, 0, 0, 5, 10, 200)
+        self.s.retirerRobot(robot1)
+        self.assertEqual(self.s.getNombreDeRobots(),0)
+        
     def TestGetRobot(self):
         robot1 = Robot("Andrew", 0, 0, 0, 5, 10, 200)
         self.assertEqual(self.s.getRobot(1),robot1)
 
     def TestRetirerRobotId(self):
         self.s.retirerRobotId(1)
-        robot2 = Robot("Tristan", 5, 5, 0, 5, 10, 200)
-        self.assertEqual(self.s.getNombreDeRobots(),1)
-        self.assertEqual(self.s.getRobot(1),robot2)
+        self.assertEqual(self.s.getNombreDeRobots(),0)
 
     
 

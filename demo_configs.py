1bf38b0224cf94897c7113c1bff206a62390b797
Maxime Dautriche <maxdautriche@gmail.com>
2023-02-22 10:01:33 +0100

    Correction de demo_config.py

--- demo_configs.py
+++ demo_configs.py
@@ -3,7 +3,7 @@ import os
 
 
 #Instantiation du controleur
-controleur = driftator.controleur()
+controleur = driftator.ia.controleur()
 
 #Definition de la taille du terrain
 tailleTerrainX = 510
@@ -24,13 +24,13 @@ while select < 1 or select > i:
     select = int(input("Choisissez la configuration à charger: "))
 
 
-simulation = driftator.chargerJson("config/" + os.listdir('config')[select-1], dT)
+simulation = driftator.simulation.chargerJson("config/" + os.listdir('config')[select-1], dT)
 
 #Initialisation de l'affichage
-affichage = driftator.Affichage(simulation, controleur, 30, 1.5, False)
+affichage = driftator.affichage.Affichage(simulation, controleur, 30, 1.5, False)
 
 #Initialisation du controleur
-implem = driftator.implemSimulation(simulation.getRobot(0), simulation)
+implem = driftator.ia.implemSimulation(simulation.getRobot(0), simulation)
 controleur.changerImplementation(implem)
 
 #Start des threads de la simulation

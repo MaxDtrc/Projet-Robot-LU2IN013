f5193c8df8ffb3ee10514cefade3cd517e4dae75
Maxime DAUTRICHE <maxdautriche@gmail.com>
2023-03-25 15:24:01 +0100

    Correction du main

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import driftator
-import driftator_sim
-
 #Variables d'affichage (1 = 2d, 2 = 3d)
 simView = 1
-
 strategie = "test2.ia"
 
 try:
+    import driftator
     driftator.start(strategie)
 
 except ImportError:
+    import driftator_sim
     driftator_sim.start(strategie, simView)

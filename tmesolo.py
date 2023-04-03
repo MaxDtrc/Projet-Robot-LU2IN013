15f6f3a5a0a89279f14d068ba0eb5dd30a042f02
guillmaster <96233870+guillmaster@users.noreply.github.com>
2023-04-03 12:26:06 +0200

    Add files via upload

---
+++ tmesolo.py
@@ -0,0 +1,39 @@
+q1.1()
+main.py
+deja la configuration de base avec config_obst.json dans le fichier config et config dans driftator_sim.start(strategie, config, simView)
+
+q1.2()
+affichage.py
+Modification dans driftator_sim/affichage.py l25 
+COULEUR_OBSTACLES = (255, 165, 0)
+
+q1.3()
+Modification dans driftator_sim/affichage.py l126
+def dessine(b):
+        if(b==True):
+            self._afficherTrace=True
+        else:
+            self._afficherTrace=False 
+
+
+et de driftator_sim/start.py(l21)
+
+q1.4()
+Ajout de testhexagone.ia dans demo_ia
+Modification du main
+
+q2.1()
+Ajout de 1.ia dans demo_ia 
+modif du main
+
+q2.2()
+Ajout de 0.ia dans demo_ia
+modif du main
+
+q2.3()
+Ajout de 01.ia dans demo_ia
+modif du main
+
+q2.4()
+Ajout de 0101.ia dans demo_ia
+modif du main

73fe1fe8da4aae9ffa667acec47dd929de61c4be
guillmaster <96233870+guillmaster@users.noreply.github.com>
2023-04-03 12:22:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,25 @@ config = "config/config_obst.json"
 config2 = "config/config_sans_obst.json"
 strategie = "demo_ia/testhexagone.ia"
 
+#q2.1
+strategie2 = "demo_ia/1.ia"
+
+#q2.2
+strategie3 = "demo_ia/0.ia"
+
+#q2.3
+strategie4 = "demo_ia/01.ia"
+
+#q2.5
+strategie5 = "demo_ia/0101.ia"
+config3 = "config/config3.json"
+
 try:
     import driftator
-    driftator.start(strategie)
+    driftator.start(strategie2)
 
 except ImportError:
     import driftator_sim
-    driftator_sim.start(strategie, config2, simView)
+    driftator_sim.start(strategie2, config3, simView)
+
+

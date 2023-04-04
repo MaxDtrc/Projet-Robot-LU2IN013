0e3df8ab361022644bd9b79155f870ca9a7b5687
Maxime DAUTRICHE <maxdautriche@gmail.com>
2023-04-04 16:15:50 +0200

    Possibilité de passer les paramètres par arguments dans main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
+import sys
 import driftator
 
-simView = 1
-strategie = "demo_ia/test2.ia"
-config = "config/config_obst.json"
+simView = int(sys.argv[1]) if len(sys.argv) >= 1 else 1
+strategie = sys.argv[2] if len(sys.argv) >= 3 else "main.ia"
+config = sys.argv[3] if len(sys.argv) >= 4 else "config/config_obst.json"
 
 try:
     driftator.startRobot(strategie)
 except ImportError:
     driftator.startSimulation(strategie, config, simView)
-
-
-
-    

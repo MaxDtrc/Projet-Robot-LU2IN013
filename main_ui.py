176eeed322ee8d269bcc68ebabf2bd7373264f19
Maxime DAUTRICHE <maxdautriche@gmail.com>
2023-05-19 18:08:36 +0200

    ajout de la possibilité de créer des nouvells IA/config

--- main_ui.py
+++ main_ui.py
@@ -51,20 +51,26 @@ screen.fill((255,255,255))
 
 #Menu des configs
 cfg_list = [f for f in listdir("config") if isfile(join("config", f))]
+cfg_list.append("+")
 cfg_page = 0
 cfg_selected = 0
+cfg_writing = False
 
 #Menu des IA
 ia_list = [f for f in listdir("demo_ia") if isfile(join("demo_ia", f))]
+ia_list.append("+")
 ia_page = 0
 ia_selected = 0
+ia_writing = False
 
 #Menu de la vue
 vue_list = ["2D", "3D"]
 vue_selected = 0
+vue_writing = False
 
 ctrl = False
 
+current_title = ""
 
 def show_menu():
     """
@@ -78,7 +84,7 @@ def show_menu():
 
     #Affichage des menus
     
-    for x, y, selected, lst, titre, clr, clr_selected in zip([cfg_menu[0], ia_menu[0], vue_menu[0]], [cfg_menu[1], ia_menu[1], vue_menu[1]], [cfg_selected, ia_selected, vue_selected], [cfg_list, ia_list, vue_list], ["Choix de la config", "Choix de l'IA", "Choix de la vue"], [clr_cfg_cases, clr_ia_cases, clr_vue_cases], [clr_cfg_selected, clr_ia_selected, clr_vue_selected]):
+    for x, y, selected, lst, titre, clr, clr_selected, writing in zip([cfg_menu[0], ia_menu[0], vue_menu[0]], [cfg_menu[1], ia_menu[1], vue_menu[1]], [cfg_selected, ia_selected, vue_selected], [cfg_list, ia_list, vue_list], ["Choix de la config", "Choix de l'IA", "Choix de la vue"], [clr_cfg_cases, clr_ia_cases, clr_vue_cases], [clr_cfg_selected, clr_ia_selected, clr_vue_selected], [cfg_writing, ia_writing, vue_writing]):
         text = pygame.font.Font('freesansbold.ttf', font_size).render(titre, False, (0, 0, 0))
         screen.blit(text, (x - text.get_width()/2, y - 35 - text.get_height()/2))
 
@@ -87,7 +93,10 @@ def show_menu():
             pygame.draw.rect(screen, clr, (x - w/2, y + (h+10) * i - h/2, w, h), border_radius=6)
 
             #Affichage du texte
-            text = pygame.font.Font('freesansbold.ttf', font_size).render(lst[i].split('.')[0], True, (0, 0, 0))
+            if i == min(5, len(lst)) - 1 and writing:
+                text = pygame.font.Font('freesansbold.ttf', font_size).render(current_title, True, (0, 0, 0))
+            else:
+                text = pygame.font.Font('freesansbold.ttf', font_size).render(lst[i].split('.')[0], True, (0, 0, 0))
             screen.blit(text, (x + 5 - w/2, y + (h+10) * i - text.get_height()/2))
 
     #Bouton lancer
@@ -121,12 +130,30 @@ while running:
         if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
             x, y = pygame.mouse.get_pos()
 
+            if ia_writing:
+                open("demo_ia/" + current_title + ".ia", "w").close()
+                ia_list.insert(-1, current_title + ".ia")
+                print("Nouveau fichier .ia créé")
+                ia_writing, current_title = False, ""
+            if cfg_writing:
+                with open("config/" + current_title + ".json", "w") as f:
+                    f.write("{\"terrain\":{\"sizeX\":152, \"sizeY\":152}, \"obstaclesRonds\":[], \"obstaclesRectangles\":[], \"robots\":[], \"balises\":[]}")
+                cfg_list.insert(-1, current_title + ".json")
+                print("Nouvelle config créée")
+                cfg_writing, current_title = False, ""
+
+
+
             #Menu config
             if cfg_menu[0] - w/2 < x < cfg_menu[0] + w/2 and cfg_menu[1] - h/2 < y < cfg_menu[1] + min(5, len(cfg_list)) * (h * 1.1):
                 cfg_selected = int((y - cfg_menu[1] + h/2)//((h * 1.2)))
+                if cfg_selected == len(cfg_list) - 1:
+                    cfg_writing = True
             #Menu IA
             elif ia_menu[0] - w/2 < x < ia_menu[0] + w/2 and ia_menu[1] - h/2 < y < ia_menu[1] + min(5, len(ia_list)) * (h * 1.1):
                 ia_selected = int((y - ia_menu[1] + h/2)//((h * 1.2)))
+                if ia_selected == len(ia_list) - 1:
+                    ia_writing = True
             #Menu des vues
             elif vue_menu[0] - w/2 < x < vue_menu[0] + w/2 and vue_menu[1] - h/2 < y < vue_menu[1] + 2 * (h * 1.1):
                 vue_selected = int((y - vue_menu[1] + h/2)//((h * 1.2)))
@@ -149,6 +176,10 @@ while running:
         
         #Detection de la touche CTRL
         elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_BACKSPACE and len(current_title) != 0:
+                current_title = current_title[:-1]
+            elif ia_writing or cfg_writing:
+                current_title += pygame.key.name(event.key).replace('8', '_') if pygame.key.name(event.key) in "abcdefghijklmnopqrstuvwxyz8" else ""
             if event.key == pygame.K_LCTRL:
                 ctrl = True
         elif event.type == pygame.KEYUP:
